9687bbb943db9904ec3c2047ea3973fc4f85d35a
upadsamay387 <56898833+upadsamay387@users.noreply.github.com>
2024-06-17 22:34:42 -0500

    Creating Table

--- answers.sh
+++ answers.sh
@@ -1,30 +1,36 @@
-SELECT Name FROM Students;
-SELECT * FROM Students WHERE Age > 30;
-SELECT Name FROM Students WHERE Gender='F' AND Age=30;
-SELECT Points FROM Students WHERE Name='Alex';
-INSERT INTO Students VALUES(10, 'Nabigha', '24', 'F', 500);
-UPDATE Students SET Points = 500 WHERE Name='Basma';
-UPDATE Students SET Points = 100 WHERE Name='Alex';
+1.
+select name from students;
 
-CREATE TABLE graduates(
-    ID INTEGER  PRIMARY KEY AUTOINCREMENT,
-    Name  TEXT NOT NULL UNIQUE,
-    Age INTEGER,
-	Gender TEXT,
-	Points INTEGER,
-	Graduation TEXT
+2.
+select * from students where age>30;
+
+3.
+select name from students where age=30 and gender='F';
+
+4.
+select points from students where name='Alex';
+
+5.
+insert into students values(7, 'Samay', 21, 'M', 500);
+
+6.
+update students set points=301 where name='Basma';
+
+7.
+update students set points=199 where name='Alex';
+
+8.
+create table graduates(
+  ID integer primary key autoincrement,
+  Name text not null unique,
+  Age integer,
+  Gender text,
+  Points integer,
+  Graduation text
 );
 
-INSERT INTO graduates(Name, Age, Gender, Points) SELECT Name, Age, Gender, Points FROM Students WHERE Students.Name='Layal';
-UPDATE graduates SET Graduation='08-09-2018' WHERE Name='Layal';
-DELETE FROM graduates WHERE Name='Layal';
+9.
+insert into graduates(name, age, gender, points) select name, age, gender, points from students where name='Layal';
+update graduates set graduation='08/09/2019' where name='Layal';
+delete from students where name='Layal';
 
-SELECT employees.name, employees.Company, companies.date FROM employees Inner join companies ON employees.Company=companies.name;
-SELECT employees.name FROM employees Inner join companies ON employees.Company=companies.name and companies.Date < 2000;
-SELECT companies.name FROM companies Inner join employees ON employees.Company=companies.name and employees.Role='Graphic Designer';
-
-select name from students where points=(SELECT MAX(points) from students);
-select AVG(points) from students;
-select COUNT(Name) from students where points=500;
-select Name from students WHERE Name GLOB '*s*';
-select name from students ORDER BY points DESC;
